775ce9467a9444782e1d5caa8f063ece58a2c813
info <info@info06.cshl.edu>
2019-10-16 11:21:05 -0400

    added ability to retreive references from command line for first_script.py

--- first_script.py
+++ first_script.py
@@ -1,3 +1,19 @@
 #! /usr/bin/env python3
-print("Brooke Tomlinson") #prints my name
+import sys
+
+#defines variables
+myname="Brooke Tomlinson"
+myfavcolor="blue"
+myfavanimal="hegdehog"
+
+#retrieve variables from the command line
+friend1 = sys.argv[1] 
+friend2 = sys.argv[2]
+
+
+#prints fun sentences
+#print("My name is", myname) 
+#print("My favorite color is", myfavcolor) 
+#print("My favorite animal is a", myfavanimal) 
+print("My friends' names are", friend1, "and", friend2)
 
