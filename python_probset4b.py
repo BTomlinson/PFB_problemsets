b4741a2a2a5feca8b75712eefeceafe04aeae188
info <info@info06.cshl.edu>
2019-10-17 20:57:01 -0400

    ammended python_prob4b.py to include through question #2

---
+++ python_probset4b.py
@@ -0,0 +1,28 @@
+#! /usr/bin/env  python3
+
+
+#save a list of taxa
+taxa = ['sapiens','erectus','neanderthalensis']
+
+#checking what type taxa is:
+print('Taxo is a', type(taxa))
+
+#split taxa into individual words and print the results
+taxa_it = iter(taxa)
+for taxon in taxa_it:
+	print(taxon)
+
+#splitting taxa list based on commas, must convert into a string first
+species = ', '.join(taxa)
+print(species)
+print(type(species))
+
+#show me what the first element in the species string is
+print(species[1])
+#SURPRISE YOU CANT, ITS A STRING SO YOU JUST GET 'a' which is the [1] index position
+
+#sort the taxon LIST alphabetically
+print(sorted(taxa))
+
+
+
