d4c9306ad4d67fdb84a78b87f0a29b417c3fa38d
info <info@info06.cshl.edu>
2019-10-18 16:47:30 -0400

    added python_probset7.py with answers though question 2

---
+++ python_probset7.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+
+#download specialized re functions
+
+import re
+
+#test if 'nobody' is in Python_07_nobody.txt. first convert to a string because the document is actually a scary input file type. ew. then search the new string for 'Nobody'
+
+namelist = open('Python_07_nobody.txt','r')
+poem  = namelist.read()
+
+if re.search(r'Nobody',poem):
+	print('Nobody home!')
+
+#ok so nobody is home. lets change nobody to Glen Coco
+
+newpoem = re.sub('Nobody','Glen Coco', poem)
+print(newpoem)
+
+
