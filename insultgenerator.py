532eef9d5217e411f0f29bdafc31e9758e6921f5
info <info@info06.cshl.edu>
2019-10-18 20:11:34 -0400

    added insultgenerator.py

---
+++ insultgenerator.py
@@ -0,0 +1,28 @@
+#! usr/bin/env python3
+import sys
+import math
+
+adj = int(sys.argv[1])
+noun = int(sys.argv[2])
+
+#sentence intro
+print("You are a")
+
+if adj = 1:
+	print("silly")
+
+elif adj = 2:
+	print("not so smart")
+
+elif adj = 3:
+		print("robust")
+
+#sentence end
+
+if noun = 1:
+	print("goose")
+elif noun = 2:
+	print("badger")
+elif noun = 3:
+	print("whale")
+ 
