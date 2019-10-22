35edd75170fea0909ca9e5c1f9df42dbf7800f67
info <info@info06.cshl.edu>
2019-10-21 21:00:32 -0400

    finished python probset 8, or all that i am currently willing to do. saved in python_probset8b.py

--- python_probset8b.py
+++ python_probset8b.py
@@ -39,10 +39,19 @@ print(codons)
 
 
 #how about the next reading frame just for fun?
-codonsframe2=[]
-for dna in listofsequences:
-	dna2=dna[1:]
-	codonsframe2.append(re.findall(r'(.{3})',dna2))
-print(codonsframe2)
+#codonsframe2=[]
+#for dna in listofsequences:
+	#dna2=dna[1:]
+	#codonsframe2.append(re.findall(r'(.{3})',dna2))
+#print(codonsframe2)
+#the above code works but silenced to keep working with first reading frame
 
+
+#instead of saving codons as list of a list from first reading frame, need to change code so it saves them with just a space between them
+codons=[]
+for dna in listofsequences:
+  codons.append(re.findall(r'(.{3})',dna)) #finds the first 3, next 3... adds to list
+codonsgrouped=[]
+for dna2 in codons:
+	print(' '.join(dna2))
 
